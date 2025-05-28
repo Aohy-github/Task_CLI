0f4f16cbc8b8a8b14546be5064717cf5a7d98ecf
linux_serves <546407791@qq.com>
2025-05-28 12:51:59 +0800

    merge

--- module/task/task.cpp
+++ module/task/task.cpp
@@ -3,9 +3,23 @@
 //
 
 #include "task.h"
+#include <ctime>
 
+std::string get_time()
+{
+    time_t timeTemp;
+    time(&timeTemp);
+    struct tm *myTime = localtime(&timeTemp);
+    std::string temp = asctime(myTime);
+    temp.pop_back();
+    return temp;
+}
 
 bool TASK::set_description(std::string&& _des){
+    time_t timestamp;
+    time(&timestamp);
+    struct tm * myTime = localtime(&timestamp);
+
     if (_des.empty())
     {
         std::cerr << "Error: text connot be empty." << std::endl;
@@ -13,5 +27,7 @@ bool TASK::set_description(std::string&& _des){
         return false;
     }
     description = std::move(_des);
+
+    updateAt = get_time();
     return true;
 }
--- module/task/task.h
+++ module/task/task.h
@@ -7,16 +7,38 @@
 
 #include <string>
 #include <iostream>
+#include <sstream>
+
+std::string get_time();
 class TASK {
 
 public:
-    TASK(std::string _des , int _id , std::string _cre_at) : description(_des) , id(_id) , createAt(_cre_at),currentState(TODO) {};
+    TASK(std::string _des , int _id , std::string _cre_at) : description(_des) , id(_id) , createAt(_cre_at),currentState(TODO),updateAt(_cre_at) {
+        currentState = DONE;
+        // std::cout << "this point is :" << this << std::endl;
+    };
     std::string get_description(){ return description; };
     std::string get_createAt(){return createAt; };
     std::string get_updateAt(){return updateAt; };
     int get_id(){return id;};
     bool set_description(std::string&& _des);
-
+    std::string to_json() const
+    {
+        // 创建一种字符串转化方式
+        std::ostringstream oss;
+        oss <<"{\n"
+            <<"  \"id\":" << this->id << ",\n"
+            <<"  \"description\":\"" << this->description << "\",\n"
+            <<"  \"createAt\":\"" << this->createAt << "\",\n"
+            <<"  \"updateAt\":\"" << this->updateAt << "\" ,\n"
+            <<"  \"State\":\"" << this->currentState << "\" \n"
+            <<"}";
+        return oss.str();
+    }
+    void to_string() const
+    {
+        std::cout << this << std::endl;
+    }
 private:
     std::string description;
     int id;
