00e36e6a3cc536af26f2ffb579620387abfc244d
aohy_w11 <546407791@qq.com>
2025-05-28 10:45:43 +0800

    need create regex

--- main.cpp
+++ main.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
+#include "spdlog/spdlog.h"
+#include "module/file/FILE.h"
+#include <spdlog/sinks/rotating_file_sink.h>
 
-int main(int n , char* argv){
+static std::string JsonFilePath;
+static void DirInit()
+{
+    bool bo = std::filesystem::create_directory("jsonFile");
+    bool bo2 = std::filesystem::create_directory("log");
+
+    static auto logger = spdlog::rotating_logger_mt("rot_logger" , "log/log.txt", 1024*1024*5,2);
+    // logger->set_pattern("[%Y-%m-%d %H:%M:%S.%e] [%l] %v");
+    logger->set_pattern("[%m-%d %H:%M:%S.%e] [%l] %v");
+    spdlog::set_default_logger(logger);
+
+    if(bo)
+    {
+        spdlog::info("jsonFile directory success");
+    }else
+    {
+        spdlog::info("jsonFile exist!");
+    }
+    if(bo2)
+    {
+        spdlog::info("log directory success");
+    }else
+    {
+        spdlog::info("log exist!");
+    }
+    JsonFilePath = "jsonFile/";
+
+}
+int main(int arg , char** argv){
+    DirInit();
+
+
+    std::cout << "Hello" << std::endl;
+    std::string fileName = "test.json";
+
+
+    std::string filePath = JsonFilePath + fileName;
+
+    IO_TaskFile io_taskFile(filePath);
+    io_taskFile.readJsonFile(filePath);
+
+    std::vector<TASK> tasks;
+    for(int i = 0; i < 10 ; i ++){
+        tasks.push_back(TASK("thi is a new taks : " + std::to_string(i) , i ,get_time()));
+        // tasks[i].to_string();
+    }
+
+    io_taskFile.writeJsonFile(filePath, tasks);
 
     return 0;
 }
--- module/file/FILE.cpp
+++ module/file/FILE.cpp
@@ -3,3 +3,56 @@
 //
 
 #include "FILE.h"
+
+bool IO_TaskFile::writeJsonFile(std::string filePath, std::vector<TASK> data) {
+    o_file.open(filePath, std::ios::app);
+
+    if (!o_file.is_open()) {
+        spdlog::error("File open failed: {}", filePath);
+        return false;
+    }
+    o_file << "{\n"; // Start of JSON array
+    o_file << "  \"tasks\": "; // Key for the array of tasks
+    o_file << "[\n"; // Start of JSON array
+    for (const auto& task : data) {
+        o_file << "  " << task.to_json();
+        if (&task != &data.back()) {
+            o_file << ",\n";
+        } else {
+            o_file << "\n";
+        }
+    }
+    o_file << "]\n"; // End of JSON array
+    o_file << "}\n"; // End of JSON array
+
+    o_file.close();
+    return true;
+}
+
+enum class Pro_STATE{
+    WAIT_FOR_ARRAY,
+    IN_ARRAY,
+    IN_OBJECT,
+    DONE
+};
+std::regex list_pa("\"");
+std::vector<TASK> IO_TaskFile::readJsonFile(std::string filePath) {
+    std::vector<TASK> tasks;
+    i_file.open(filePath);
+    if (!i_file.is_open()) {
+        spdlog::error("File open failed: {}", filePath);
+        return tasks;
+    }
+    std::string line;
+    Pro_STATE state = Pro_STATE::WAIT_FOR_ARRAY;
+    while (std::getline(i_file, line)) {
+        line.erase(remove_if(line.begin() , line.end() ,isspace) , line.end());
+        spdlog::info("Read line: {}", line);
+        if(state = Pro_STATE::WAIT_FOR_ARRAY && ){
+
+        }
+    }
+
+    i_file.close();
+    return tasks;
+}
